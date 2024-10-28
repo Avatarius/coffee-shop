e0ca6003e773c099d016a8436e30c5c8f77b5af2
Chingis Choybsonov <avatarius93@gmail.com>
2024-10-29 02:58:15 +0300

    gh pages

--- src/services/slices/order.ts
+++ src/services/slices/order.ts
@@ -1,18 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { IOrder } from "../../utils/types";
+import { postOrder } from "../thunk/order";
 
 interface IOrderInitialState {
   order: IOrder | null;
 }
 
 const initialState: IOrderInitialState = {
-  order: null
+  order: null,
 };
 
 const orderSlice = createSlice({
   name: "order",
   initialState,
-  reducers: {
-
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(postOrder.fulfilled, (state, action) => {
+      console.log('order posted');
+    })
   }
 });
+
+
+export { orderSlice };
